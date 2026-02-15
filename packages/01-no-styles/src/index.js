216f00b2a31b765345bfd96dda4d587d58994b16
Karl Horky <karl.horky@gmail.com>
2026-02-15 17:40:19 +0100

    Add missing import

--- packages/01-no-styles/src/index.js
+++ packages/01-no-styles/src/index.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { StrictMode, useEffect, useState } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 
@@ -15,7 +15,7 @@ function Home() {
 }
 
 createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <StrictMode>
     <Home />
-  </React.StrictMode>,
+  </StrictMode>,
 );
