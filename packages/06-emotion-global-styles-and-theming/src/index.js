5af7aa75f3274262e1a4e7355d46a346e5ad29ec
Karl Horky <karl.horky@gmail.com>
2026-02-15 18:14:32 +0100

    Add missing import

--- packages/06-emotion-global-styles-and-theming/src/index.js
+++ packages/06-emotion-global-styles-and-theming/src/index.js
@@ -1,5 +1,5 @@
 import { Global, ThemeProvider } from '@emotion/react';
-import { useEffect, useState } from 'react';
+import { StrictMode, useEffect, useState } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 import { globalStyles } from './components';
